39120b485f87274e94cf68baf36ff6b076b93c39
Elaine Ho <elaineh4@uci.edu>
2020-11-11 23:41:14 -0800

    Added imageCollector.py

---
+++ code/imageCollector.py
@@ -0,0 +1,137 @@
+from __future__ import print_function
+# ------------------------------------------------------------------------------------------------
+# Copyright (c) 2016 Microsoft Corporation
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+# associated documentation files (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge, publish, distribute,
+# sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+# NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# ------------------------------------------------------------------------------------------------
+
+from builtins import range
+import MalmoPython
+import os
+import sys
+import time
+import numpy as np
+from pynput.keyboard import Key, Controller
+
+if sys.version_info[0] == 2:
+    sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
+else:
+    import functools
+    print = functools.partial(print, flush=True)
+
+x = np.random.randint(-1000, 1000)
+z = np.random.randint(-1000, 1000)
+print(x,z)
+
+missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+            <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            
+              <About>
+                <Summary>Collector</Summary>
+              </About>
+              
+              <ServerSection>
+                <ServerInitialConditions>
+                  <Time>
+                    <StartTime>12000</StartTime>
+                    <AllowPassageOfTime>false</AllowPassageOfTime>
+                  </Time>
+                  <Weather>rain</Weather>
+                  <AllowSpawning>false</AllowSpawning>
+                  <AllowedMobs></AllowedMobs>
+                </ServerInitialConditions>
+                <ServerHandlers>
+                  <FileWorldGenerator src="" />
+                  <ServerQuitWhenAnyAgentFinishes/>
+                </ServerHandlers>
+              </ServerSection>
+              
+              <AgentSection mode="Creative">
+                <Name>yaboi</Name>
+                <AgentStart>
+                  <Placement x="{}" y="100" z="{}" yaw="90"/>
+                </AgentStart>
+                <AgentHandlers>
+                  <ObservationFromFullStats/>
+                  <ContinuousMovementCommands turnSpeedDegs="180"/>
+                  <ChatCommands/>
+                </AgentHandlers>
+              </AgentSection>
+            </Mission>'''.format(x, z)
+
+# Create default Malmo objects:
+keyboard = Controller()
+agent_host = MalmoPython.AgentHost()
+try:
+    agent_host.parse( sys.argv )
+except RuntimeError as e:
+    print('ERROR:',e)
+    print(agent_host.getUsage())
+    exit(1)
+if agent_host.receivedArgument("help"):
+    print(agent_host.getUsage())
+    exit(0)
+
+my_mission = MalmoPython.MissionSpec(missionXML, True)
+my_mission_record = MalmoPython.MissionRecordSpec()
+
+# Attempt to start a mission:
+max_retries = 3
+for retry in range(max_retries):
+    try:
+        agent_host.startMission( my_mission, my_mission_record )
+        break
+    except RuntimeError as e:
+        if retry == max_retries - 1:
+            print("Error starting mission:",e)
+            exit(1)
+        else:
+            time.sleep(2)
+
+# Loop until mission starts:
+print("Waiting for the mission to start ", end=' ')
+world_state = agent_host.getWorldState()
+while not world_state.has_mission_begun:
+    print(".", end="")
+    time.sleep(0.1)
+    world_state = agent_host.getWorldState()
+    for error in world_state.errors:
+        print("Error:",error.text)
+
+print()
+print("Mission running ", end=' ')
+
+
+while world_state.is_mission_running:
+  time.sleep(5)
+  keyboard.press(Key.f2)
+  keyboard.release(Key.f2)
+  time.sleep(2)
+  agent_host.sendCommand("chat /weather clear")
+  time.sleep(5)
+  keyboard.press(Key.f2)
+  keyboard.release(Key.f2)
+  time.sleep(2)
+  x = np.random.randint(-100000, 100000)
+  z = np.random.randint(-100000, 100000)
+  print(x,z)
+  agent_host.sendCommand("chat /weather rain")
+  agent_host.sendCommand("chat /tp yaboi " + str(x) + " 100 " + str(z))
+
+
+print()
+print("Mission ended")
+# Mission has ended.
